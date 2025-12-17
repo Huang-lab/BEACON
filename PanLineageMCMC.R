7db1ca57cd3c1295e8acbb71f0df5544beb78c77
Abdulkadir Elmas <ae2321@columbia.edu>
2025-12-17 12:52:47 +0300

    Update PanLineageMCMC.R

--- PanLineageMCMC.R
+++ PanLineageMCMC.R
@@ -13,8 +13,6 @@
 #   - Supplementary data (Table S2: normalized protein expressions) from Nusinow et al. paper (doi.org/10.1016/j.cell.2019.12.023) @ https://www.cell.com/cms/10.1016/j.cell.2019.12.023/attachment/3709dedc-3a01-4e1d-ab4c-82597295c5d2 
 #   - CRISPR_gene_effect.csv file (further gzipped) @ https://figshare.com/articles/dataset/DepMap_22Q2_Public/19700056/2?file=34990036
 # _|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|
-# ml R jags/4.3.0
-# R
 rm(list = ls(all.names = TRUE))
 options(java.parameters = '-Xmx8000m')
 library(openxlsx)
